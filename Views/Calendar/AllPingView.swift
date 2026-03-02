ba5b77bfeda7ba81828337a6a83e7a96fac7899b
David <Dponskiy@gmail.com>
2026-03-01 23:09:05 -0500

    updated notifications and calendar edits

--- Views/Calendar/AllPingView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  AllPingView.swift
-//  Orca
-//
-//  Created by David Piliponskiy on 2/25/26.
-//
-
--- Views/Calendar/DayDetailView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  DayDetailView.swift
-//  Orca
-//
-//  Created by David Piliponskiy on 2/25/26.
-//
-
