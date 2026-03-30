0ded32568083716d6a7aa7d8a8f389a2b4bc564f
David <Dponskiy@gmail.com>
2026-03-30 16:53:45 -0400

    people profile! and task bugs

---
+++ People/GiftItem.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Orca
+//
+//  Created by David Piliponskiy on 3/29/26.
+//
+
---
+++ People/Person.swift
@@ -0,0 +1,7 @@
+//
+//  People.swift
+//  Orca
+//
+//  Created by David Piliponskiy on 3/29/26.
+//
+
